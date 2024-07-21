b2582223dead7c5814d969d239d4b3dedd8e42d8
Blue <lprwasunavailable@gmail.com>
2024-07-21 22:50:53 +0200

    moved to own file for sanity sake

---
+++ src/app/utils/misc.ts
@@ -0,0 +1,10 @@
+import { InputData } from "./types";
+
+export const individualTemplate: InputData = {
+	"Asset Name": "Name",
+	"Team": "Country - Club Name",
+	"ID LEFT OUTSIDE": "ID LEFT OUTSIDE",
+	"ID LEFT INSIDE": "ID LEFT INSIDE",
+	"ID RIGHT OUTSIDE": "ID RIGHT OUTSIDE",
+	"ID RIGHT INSIDE": "ID RIGHT INSIDE",
+};
