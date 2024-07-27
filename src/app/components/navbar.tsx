320df4ff8581337f7a50071020d93bdb05c879fb
Blue <lprwasunavailable@gmail.com>
2024-07-27 23:57:40 +0200

    improved layout, updated navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,14 @@
 const Navbar = () => {
 	return (
 		<nav>
-			<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-				v0.4
-			</p>
+			<div className="fixed left-0 top-0 px-8 flex w-full justify-between items-center leading-none dark:text-white/20 text-zinc-800/20 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-slate-700/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:px-8 lg:py-4 lg:dark:bg-zinc-800/30">
+				<div className="icon text-4xl select-none" draggable={false}>
+					&#9863;
+				</div>
+				<div className="version-number">
+					v0.5
+				</div>
+			</div>
 		</nav>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AppManager from "./components/appManager";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between py-16 px-16">
+		<main className="flex min-h-screen flex-col items-center justify-between py-32 lg:py-12 px-16">
 			<div className="flex flex-col items-center justify-between font-mono text-sm">
 				<AppManager />
 			</div>
