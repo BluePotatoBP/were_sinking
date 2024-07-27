191b1ff65fa9c0fe23a806d98b366a258d301b85
Blue <lprwasunavailable@gmail.com>
2024-07-28 00:01:50 +0200

    shorter template text for clarity

--- src/app/utils/misc.ts
+++ src/app/utils/misc.ts
@@ -1,10 +1,10 @@
 import { InputData } from "./types";
 
 export const individualTemplate: InputData = {
-	"Asset Name": "Name",
-	"Team": "Country - Club Name",
-	"ID LEFT OUTSIDE": "ID LEFT OUTSIDE",
-	"ID LEFT INSIDE": "ID LEFT INSIDE",
-	"ID RIGHT OUTSIDE": "ID RIGHT OUTSIDE",
-	"ID RIGHT INSIDE": "ID RIGHT INSIDE",
+	"Asset Name": "NAME",
+	"Team": "COUNTRY - CLUB",
+	"ID LEFT OUTSIDE": "LEFT-OUT",
+	"ID LEFT INSIDE": "LEFT-IN",
+	"ID RIGHT OUTSIDE": "RIGHT-OUT",
+	"ID RIGHT INSIDE": "RIGHT-IN",
 };
