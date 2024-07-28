9b99dceab2339dfb87854959ed4d450e1bb5bfcc
Blue <lprwasunavailable@gmail.com>
2024-07-28 22:03:33 +0200

    new shoe page parser component

---
+++ src/app/components/pageParser.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useRef } from 'react';
+import { useDebounce } from '@/app/utils/hooks';
+import { ParsedPageData } from '@/app/utils/types';
+
+
+interface ParserProps {
+	onParseComplete: (parsedData: ParsedPageData) => void;
+}
+
+const Parser: React.FC<ParserProps> = ({ onParseComplete }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const parseText = useCallback((text: string): ParsedPageData => {
+		const patterns = {
+			playerName: /Player([\s\S]*?)Club/,
+			clubName: /Club([\s\S]*?)League/,
+			positions: /(LEFT|RIGHT) \((OUTSIDE|INSIDE)\)([\s\S]*?)(?=(LEFT|RIGHT) \((OUTSIDE|INSIDE)\)|$)/g,
+		};
+
+		const playerName = (text.match(patterns.playerName) || [])[1]?.trim() || '';
+		const clubName = (text.match(patterns.clubName) || [])[1]?.trim() || '';
+
+		const positions: ParsedPageData['positions'] = {};
+		let match;
+		while ((match = patterns.positions.exec(text)) !== null) {
+			const [, side, inOut, content] = match;
+			const key = `${side} ${inOut}`;
+			const idMatch = content.match(/Position 1:\s*ID\s*([\s\S]*?)(?=Position \d+:|$)/);
+			// If needed in the future, also uncomment type in types.ts
+			/* const flagMatch = content.match(/Position 2:\s*Flag\s*([\s\S]*?)(?=Position \d+:|$)/); */
+
+			positions[key] = {
+				id: idMatch ? idMatch[1].trim() : ''
+				/* flag: flagMatch ? flagMatch[1].trim() : '', */
+			};
+		}
+
+		return { playerName, clubName, positions };
+	}, []);
+
+	const handleInputChange = useDebounce((input: string) => {
+		const parsed = parseText(input);
+		onParseComplete(parsed);
+		if (inputRef.current) inputRef.current.value = '';
+	}, 200);
+
+	return (
+		<div className="selection-parser-container flex flex-col gap-4 text-black">
+			<input className="flex p-2 border rounded w-14" ref={inputRef} onChange={(e) => handleInputChange(e.target.value)} placeholder="Fill" title='Paste the entire Shoe order page and automatically populate current transfer.' />
+		</div>
+	);
+};
+
+export default Parser;
