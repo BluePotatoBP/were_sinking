7d1d14e154265c7fa6541acbd207af07457d770c
Blue <lprwasunavailable@gmail.com>
2024-08-04 03:30:14 +0200

    abstracted file upload to own component

--- src/app/components/appManager.tsx
+++ src/app/components/appManager.tsx
@@ -1,10 +1,11 @@
 'use client';
 import XlsxTableParser from "@/app/components/xlsxTableParser";
 import TransferEditor from "@/app/components/transferEditor";
+import FileSelectorButton from "@/app/components/fileSelectorButton";
+
 import { InputData } from '@/app/utils/types';
 import { individualTemplate } from '@/app/utils/misc';
-import { ChangeEvent, useState } from "react";
-import { WorkBook, WorkSheet, read, utils } from "xlsx";
+import { useCallback, useState } from "react";
 
 import { MdFormatLineSpacing } from "react-icons/md";
 import { TbLayoutSidebarLeftExpandFilled, TbLayoutSidebarRightExpandFilled } from "react-icons/tb";
@@ -19,49 +20,16 @@ const AppManager: React.FC = () => {
 
 	const handleCompactClick = () => setIsCompact(!isCompact);
 	const handleExpand = () => setIsExpanded(!isExpanded);
+	const handleDataUpdate = (newDataOrUpdater: React.SetStateAction<InputData[]>) =>
+		currentTab === "INDIVIDUAL" ? setIndividualTransfers(newDataOrUpdater) : setFileData(newDataOrUpdater);
 
-	const handleFileUpload = (input: ChangeEvent<HTMLInputElement>) => {
-		const reader = new FileReader();
-		if (input.target.files && input.target.files.length > 0) {
-			reader.readAsArrayBuffer(input.target.files[0] as Blob);
-			reader.onload = (loaded) => {
-				const data = loaded.target?.result;
-				const workbook: WorkBook = read(data, { type: 'binary' });
-				const sheetName = workbook.SheetNames[0];
-				const sheet: WorkSheet = workbook.Sheets[sheetName];
-				const parsedData: InputData[] = utils.sheet_to_json(sheet);
-
-				// Filter data to get rid of emoji and symbols
-				const regexStr = /[\p{L}\d!@#$%^&*()?.,<>\/\\'":;+_\-\+~`\|{}\[\]=]+/gmiu;
-
-				const filteredData: InputData[] = parsedData.map((item) => {
-					const filteredItem: InputData = {};
-					Object.keys(individualTemplate).forEach((field) => {
-						const value = item[field];
-						if (value !== undefined) {
-							const matchedValue = value.toString().match(regexStr)?.join(' ');
-							if (matchedValue) {
-								filteredItem[field] = isNaN(Number(matchedValue)) ? matchedValue : Number(matchedValue);
-							}
-						} else {
-							filteredItem[field] = ""; // Set empty string for missing fields
-						}
-					});
-					return filteredItem;
-				});
-				setFileData(filteredData);
-				setCurrentTab("MASTERFILE");
-			};
-		}
-	};
-
-	const handleDataUpdate = (newDataOrUpdater: React.SetStateAction<InputData[]>) => {
-		if (currentTab === "INDIVIDUAL") {
-			setIndividualTransfers(newDataOrUpdater);
-		} else {
-			setFileData(newDataOrUpdater);
-		}
-	};
+	const handleFileSelect = useCallback((data: InputData[]) => {
+		setCurrentTab("MASTERFILE");
+		// Apparently this is necessary to ensure the state updates
+		setTimeout(() => {
+			setFileData(data);
+		}, 0);
+	}, []);
 
 	const currentData = currentTab === "INDIVIDUAL" ? individualTransfers : fileData;
 
@@ -82,10 +50,10 @@ const AppManager: React.FC = () => {
 				</div>
 				<TransferEditor data={currentData} tabType={currentTab} onDataUpdate={handleDataUpdate} />
 			</div>
-			<div className={`right flex flex-col gap-4 w-full ${isExpanded ? 'lg:max-w-[50vw]' : 'lg:max-w-[32vw] 2xl:max-w-[22vw]'}`}>
+			<div className={`right flex flex-col gap-4 w-full ${isExpanded ? 'lg:max-w-[50vw]' : 'lg:max-w-[17vw] 2xl:max-w-[22vw]'}`}>
 				<div className="top-container flex flex-row justify-between items-center dark:bg-slate-800 bg-slate-400 p-4 rounded-2xl">
 					<div className="input-container dark:text-white text-slate-600">
-						<input type="file" accept='.xlsx, .xls' onChange={handleFileUpload} className="leading-none" />
+						<FileSelectorButton onFileSelect={handleFileSelect} />
 					</div>
 					<div className="controls-container p-0 m-0 leading-none">
 						{fileData && Array.isArray(fileData) ? fileData.length > 0 && (
@@ -96,7 +64,7 @@ const AppManager: React.FC = () => {
 						) : null}
 					</div>
 				</div>
-				<XlsxTableParser data={fileData} isCompact={isCompact} isExpanded={isExpanded} key={isCompact.toString()} />
+				<XlsxTableParser data={fileData} isCompact={isCompact} key={isCompact.toString()} />
 			</div>
 		</div>
 	);
---
+++ src/app/components/fileSelectorButton.tsx
@@ -0,0 +1,61 @@
+import { ChangeEvent, useRef } from "react";
+import { WorkBook, WorkSheet, read, utils } from "xlsx";
+import { InputData } from '@/app/utils/types';
+import { individualTemplate } from '@/app/utils/misc';
+
+interface FileSelectorButtonProps {
+	onFileSelect: (data: InputData[]) => void;
+}
+
+const FileSelectorButton: React.FC<FileSelectorButtonProps> = ({ onFileSelect }) => {
+	const inputRef = useRef<HTMLInputElement | null>(null);
+
+	const handleFileUpload = async (input: ChangeEvent<HTMLInputElement>) => {
+		const reader = new FileReader();
+		if (input.target.files && input.target.files.length > 0) {
+			reader.readAsArrayBuffer(input.target.files[0] as Blob);
+			reader.onload = async (loaded) => {
+				const data = loaded.target?.result;
+				const workbook: WorkBook = read(data, { type: 'binary' });
+				const sheetName = workbook.SheetNames[0];
+				const sheet: WorkSheet = workbook.Sheets[sheetName];
+				const parsedData: InputData[] = utils.sheet_to_json(sheet);
+
+				/**
+				 * Filter data to get rid of emoji and symbols.
+				 * 
+				 * It's actually reversed, match unicode characters
+				 * like letters + most symbols and discard everything else (emoji)
+				 */
+				const regexStr = /[\p{L}\d!@#$%^&*()?.,<>\/\\'":;+_\-\+~`\|{}\[\]=]+/u;
+
+				const filteredData: InputData[] = parsedData.map((item) => {
+					const filteredItem: InputData = {};
+					Object.keys(individualTemplate).forEach((field) => {
+						const value = item[field];
+						if (value !== undefined) {
+							const matchedValue = value.toString().match(regexStr)?.join(' ');
+							if (matchedValue) {
+								filteredItem[field] = isNaN(Number(matchedValue)) ? matchedValue : Number(matchedValue);
+							}
+						} else {
+							filteredItem[field] = ""; // Set empty string for missing fields
+						}
+					});
+					return filteredItem;
+				});
+
+				onFileSelect(filteredData);
+			};
+		}
+	};
+
+	return (
+		<div className="select-file-btn">
+			<button className="text-slate-500 font-mono font-black transition-colors hover:text-white" onClick={() => inputRef.current?.click()}>Choose File</button>
+			<input type="file" id="table-input" ref={inputRef} accept='.xlsx, .xls' onChange={handleFileUpload} className="hidden" />
+		</div>
+	);
+};
+
+export default FileSelectorButton;
